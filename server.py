4f1df1bdf76543ca7fb38af95b528ecb75125982
AnliYang <anli.m.yang@gmail.com>
2016-08-31 14:36:19 -0700

    Remove hard-coded gif, replace with random Spock gif from giphy API

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 import os
+import requests
+import random
 from flask import Flask, jsonify, request
+
+from pprint import pprint
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
 @app.route('/', methods=["GET"])
 def index():
-    image_url = 'http://giphy.com/gifs/NpEE2cRs2Amkg'
+    image_url = get_gif('Spock')
     text = 'Live long and prosper.'
     episode_credit = 'Start Trek, season 2, episode 1 ("Amok Time," 1968)'
 
@@ -26,6 +30,36 @@ def index():
 
     return jsonify(response)
 
+def get_gif(search_term='Spock'):
+    """Hit up the Giphy Search API to get a random gif."""
+
+    # FIXME: currently hardcoded and not doing any parsing, substitution
+    query_term = search_term
+
+    params = {
+        # FIXME: Currently using beta key, should request actual for production.
+        'api_key': os.environ.get('GIPHY_API_KEY'),
+        'q': query_term,
+        'limit': 1,
+        'offset': 0, #results offset, defaults to 0
+        'rating': 'g', #limit results to those rated (y, g, pg, pg-13 or r)
+        'fmt': 'json', #other option is HTML, for debugging in browser
+    }
+
+    initial_response = requests.get('http://api.giphy.com/v1/gifs/search', params=params).json()
+
+    results_count = initial_response['pagination']['total_count']
+    offset = random.randrange(1, results_count-1)
+    params['offset'] = offset
+
+    final_response = requests.get('http://api.giphy.com/v1/gifs/search', params=params).json()
+    gif_url = final_response['data'][0]['images']['original']['url']
+
+    return gif_url
+
+
 if __name__ == "__main__":
     PORT = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=PORT)
+    DEBUG = "NO_DEBUG" not in os.environ
+
+    app.run(host="0.0.0.0", port=PORT, debug=DEBUG)
